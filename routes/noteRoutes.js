881fc95900a543832533df701704ffc3bbd5d29f
sairemgloria <sairemgloria1999@gmail.com>
2025-02-10 16:00:28 +0800

    Updated noteRoutes.js

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -5,4 +5,23 @@ router.get("/", (req, res) => {
   res.status(200).json({ message: "All notes" });
 });
 
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  res.status(200).json({ message: `Get notes id: ${id}` });
+});
+
+router.post("/", (req, res) => {
+  res.status(200).json({ message: "Created notes" });
+});
+
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  res.status(200).json({ message: `Notes updated id:${id}` });
+});
+
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  res.status(200).json({ message: `Notes deleted id: ${id}` });
+});
+
 module.exports = router;
