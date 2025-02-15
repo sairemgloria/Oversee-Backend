c119c8734e6ebc5a9211735aabfedde37880bede
sairemgloria <sairemgloria1999@gmail.com>
2025-02-15 14:36:19 +0800

    Added CORS for backend.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const cors = require("cors");
 const port = process.env.PORT || 3000;
 const connectDB = require("./config/db");
 const { errorHandler } = require("./middlewares/errorMiddleware");
@@ -8,6 +9,20 @@ connectDB();
 
 const app = express();
 
+/* CORS */
+app.use(
+  cors({
+    /* Allow requests from this origin */
+    origin: "http://localhost:5173",
+
+    /* Allowed HTTP methods */
+    methods: ["GET", "POST", "PUT", "DELETE"],
+
+    /* Include credentials like cookies if needed */
+    credentials: true,
+  })
+);
+
 /* Middlewares */
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -15,6 +30,7 @@ app.use(express.urlencoded({ extended: true }));
 /* Routes */
 app.use("/api/admins", require("./routes/adminRoutes"));
 
+/* Error Handler */
 app.use(errorHandler);
 
 app.listen(port, () => {
