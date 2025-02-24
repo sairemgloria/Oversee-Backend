979038b820b23e61eea6269798618a49e0179555
sairemgloria <sairemgloria1999@gmail.com>
2025-02-24 23:34:12 +0800

    Updated again departmentAdminRoutes.js fix the put request route.

--- routes/departmentAdminRoutes.js
+++ routes/departmentAdminRoutes.js
@@ -11,6 +11,12 @@ const router = express.Router();
 router.get("/", getAllDepartmentAdmin);
 router.get("/:id", getSelectedDepartmentAdmin);
 router.post("/", createDepartmentAdmin);
-router.put("/:id?", updateDepartmentAdmin);
+router.put("/", (req, res) => {
+  return res.status(400).json({
+    success: false,
+    message: "No Department Admin ID provided.",
+  });
+});
+router.put("/:id", updateDepartmentAdmin);
 
 module.exports = router;
